af0ace0b69dcfc174e19471403d405726b6cc2d8
Vladimir <sirbuvova4@gmail.com>
2021-07-01 18:11:54 +0000

    added documentation comments to ColorManager class

--- src/main/java/com/mastermind/ColorManager.java
+++ src/main/java/com/mastermind/ColorManager.java
@@ -2,17 +2,27 @@ package com.mastermind;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * It's a class that perform colors.
+ * This class have an list where hold 7 colors of rainbow.
+ */
 public class ColorManager {
+
     /**
-     * com.mastermind.ColorManager have in it's box 7 base color of rainbow
+     * List to hold colors objects
      */
     private final List<Color> allColors = new ArrayList<>();
 
+    /**
+     * In constructor call method to initialize list of {@code allColors}
+     */
     public ColorManager() {
         initBoxOfColors();
     }
 
+    /**
+     * Initialize {@code allColors} list with objects {@code Color}
+     */
     private void initBoxOfColors() {
         allColors.add(new Color('R'));
         allColors.add(new Color('G'));
@@ -23,11 +33,18 @@ public class ColorManager {
         allColors.add(new Color('V'));
     }
 
+    /**
+     * Generate random color from {@code allColors} list
+     * @return {@code Color} object
+     */
     public Color getRandomColor() {
-        int random = (int)(Math.random() * 7);
+        int random = (int)(Math.random() * 7); // get an integer number in a range [0,7)
         return allColors.get(random);
     }
 
+    /**
+     * @return {@code List} of all colors.
+     */
     public List<Color> getAllColors() {
         return allColors;
     }
